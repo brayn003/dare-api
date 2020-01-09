abd47f64dcc1731cf9f10e9114500aa7a97b4797
dhairyadgala <dhairyagala00@gmail.com>
2020-01-09 14:57:23 +0530

    fix

--- app/controllers/subscription/createMany.js
+++ app/controllers/subscription/createMany.js
@@ -3,7 +3,7 @@ const Subscription = require('../../models/Subscription');
 const controller = async (req, res) => {
     const {user, body} = req;
     const {interests} = body;
-    const toWrite = interests.map(interestId => ({ interestId, userId: user.id }));
+    const toWrite = interests.map(interestId => ({ interestId, userId: user.id, subscribed: true }));
     await Subscription.insertMany(toWrite);
     const response = await Subscription.find({interestId: {$in: interests}});
     return res.status(201).json(response);
