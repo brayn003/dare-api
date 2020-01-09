e3456e0a32f8b79c707b884b528a6b9274106006
dhairyadgala <dhairyagala00@gmail.com>
2020-01-09 14:24:27 +0530

    adding completed

--- app/controllers/user/profile.js
+++ app/controllers/user/profile.js
@@ -4,7 +4,7 @@ const Subscription = require('../../models/Subscription');
 const controller = async (req, res) => {
     const {user} = req;
     const daresAccepted = await DaresCompleted.countDocuments({ userId: user.id });
-    const daresCompleted = await DaresCompleted.countDocuments({ userId: user.id, completed: false });
+    const daresCompleted = await DaresCompleted.countDocuments({ userId: user.id, completed: true });
     const interests = await Subscription.find({userId: user.id});
     return res.status(200).json({
         user,
